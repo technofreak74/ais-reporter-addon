a9f809c4987e9df5cce2aa92a9eb037f1c0f88c1
technofreak74 <9699636+technofreak74@users.noreply.github.com>
2020-05-16 14:16:52 +0100

    Print serial port config on start

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/with-contenv bashio
+CONFIG_PATH=/data/options.json
+
+SERIAL_PORT="$(bashio::config 'serial_port')"
 
 echo AIS Reporter Starting...
+echo Using serial port: $SERIAL_PORT
 
 python3 UDPbroadcast.py
 
